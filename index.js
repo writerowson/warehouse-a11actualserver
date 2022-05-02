6d4f2b39b232c2b3d23d3366997883860d888c0e
witerowson <runurowson@gmail.com>
2022-05-02 06:42:26 +0600

    user connected

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
+
+// use middleware
+app.use(cors())
+app.use(express.json())
+
+
+const uri = "mongodb+srv://perfumeuser:EwTrTEJN2NSEGsmY@cluster0.ofq71.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('connected');
+    // perform actions on the collection object
+    client.close();
+
+});
 
 app.get('/', (req, res) => {
     res.send('Running my Node curd')
