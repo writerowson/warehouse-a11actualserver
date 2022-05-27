0c7b80298a280173d114b0cfbb7baef08acafc68
witerowson <runurowson@gmail.com>
2022-05-27 20:40:09 +0600

    update data

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Live link https://assignment11-8f26c.web.app/
 ## Stackoverflow link https://stackoverflow.com/questions/72149729/failed-to-fetch-data-from-mongodb-in-dynamic-route
 
-### This is server site of pure perfume website. Usually server site is use for store data of website  Here we use MongoDb to store data 
+### This is server site of pure perfume website. Usually server site is use for store data of website.  Here we use MongoDb to store data 
 
 
 ### Here the list of methods using in server site
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const res = require('express/lib/response');
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 // use middleware
 app.use(cors())
@@ -21,7 +22,7 @@ async function run() {
         await client.connect();
         console.log('all connected');
         const productCollection = client.db("purePerfume").collection("inventory");
-        // to load data
+        // to load all data
         app.get('/inventory', async (req, res) => {
             const query = {}
             // cursor for multiple item
@@ -30,6 +31,7 @@ async function run() {
             res.send(inventories)
             console.log(inventories);
         })
+        //  to load single data
         app.get('/inventory/:id', async (req, res) => {
             const id = req.params.id;
             console.log(id)
@@ -38,7 +40,20 @@ async function run() {
             const inventory = await productCollection.findOne(query)
             res.send(inventory)
         })
+        // to update data
+        app.put('inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedInventory = req.body;
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: updatedInventory
+            };
+            const result = await productCollection.updateOne(query, updateDoc, options);
+            res.send(result);
+        })
 
+        // for page count
         app.get('/inventoryCount', async (req, res) => {
             const query = {}
             const cursor = productCollection.find(query);
@@ -50,6 +65,22 @@ async function run() {
 
     }
 }
+app.post('/login', (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.send({ success: true })
+    if (user.email === 'user@gmail.com' && user.password === '123456') {
+        const accessToken = jwt.sign({ email: user.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+        req.send({
+            success: true,
+            accessToken: accessToken
+        })
+    }
+    else {
+        res.send({ success: false })
+    }
+})
+
 run().catch(console.dir)
 app.get('/', (req, res) => {
     res.send('Running my Node curd')
